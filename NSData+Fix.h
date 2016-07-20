50d1e33dcfb54f4f8855bacb551f0393984e647f
Chen Zhilei <chen.zhilei@im-ee.com>
2016-07-20 20:48:17 +0800

    initial commit

---
+++ NSData+Fix.h
@@ -0,0 +1,15 @@
+//
+//  NSData+Fix.h
+//  RCTNetwork
+//
+//  Created by ee on 16/6/17.
+//  Copyright © 2016年 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (Fix)
+
+- (NSString*) UTF8String;
+
+@end
